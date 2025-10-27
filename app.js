189350576450e4958f186f564371a6c608b45823
Bruno Henrique <thedevbruno@gmail.com>
2025-10-27 09:24:27 -0300

    Foi Adicionado mais funcionalidades no app.js

--- app.js
+++ app.js
@@ -1,16 +1,49 @@
 const form = document.querySelector('.form')
 const ul = document.querySelector('.todo-item')
+const buttonRemoveItem = document.querySelector('.remove-item')
+const buttonRemoveAll = document.querySelector('.remove-all')
 
 
 
+
+
+
+function refreshButtons() {
+    if (ul.children.length === 0) {
+        buttonRemoveItem.classList.add('ocult')
+        buttonRemoveAll.classList.add('ocult')
+    } else {
+        buttonRemoveItem.classList.remove('ocult')
+        buttonRemoveAll.classList.remove('ocult')
+    }
+}
+
+
+refreshButtons()
 
 form.addEventListener('submit', event => {
     event.preventDefault()
 
     const valueInput = event.target.inputValue.value
-    ul.innerHTML += `<li class="li-item"><input type="checkbox" name="valueInput" class="check-todo">${valueInput}<i class="fa-solid fa-trash"></i></li>`
+
+
+
+    if (valueInput.length > 0) {
+
+        ul.innerHTML += `<li class="li-item"><input type="checkbox" name="valueInput" class="check-todo">${valueInput}<i class="fa-solid fa-trash"></i></li>`
+        buttonRemoveItem.classList.remove('ocult')
+        buttonRemoveAll.classList.remove('ocult')
+
+    }
+
+    if (valueInput.length < 0) {
+        buttonRemoveItem.classList.add('ocult')
+        buttonRemoveAll.classList.add('ocult')
+    }
+    refreshButtons()
     form.reset()
 })
+
 
 
 ul.addEventListener('click', event => {
@@ -20,16 +53,40 @@ ul.addEventListener('click', event => {
 
         if (checked) {
             event.target.parentElement.classList.add('to-do-executado')
+
         } else { event.target.parentElement.classList.remove('to-do-executado') }
     }
 
     if (classItem.includes('fa-trash')) {
-
         event.target.parentElement.remove()
-
     }
 
 
 
 
+    refreshButtons()
+
 })
+
+
+
+
+
+buttonRemoveItem.addEventListener('click', () => {
+
+    const selectedTodo = ul.querySelectorAll('.check-todo:checked')
+
+
+    if (selectedTodo.length === 0) return
+
+    selectedTodo.forEach(input => input.parentElement.remove())
+    refreshButtons()
+})
+
+
+buttonRemoveAll.addEventListener('click', () => {
+    ul.innerHTML = ''
+    refreshButtons()
+})
+
+
